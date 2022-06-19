97533d060c8211fd0c9d39277dd51ce4ab59cb6b
George Zhou <georgezhou89@hotmail.com>
2022-06-19 15:58:54 -0400

    Added Student Model with Encryption

---
+++ app/models/student.server.model.js
@@ -0,0 +1,62 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+// encryption
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// Define a new 'StudentSchema'
+var StudentSchema = new Schema({
+    studendID:{type:String, unique:true, required: true},
+    studentFirstName: String,
+    studentLastName: String,
+    studentEmail:{type:String,
+    //email validation format
+    match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    password:{
+        type:String,
+        // Validate Password
+        validate: [
+			(password) => password && password.length > 6,
+			'Password should be longer'
+		]
+    },
+    enrollDate:{
+        type:Date,
+    },
+    graduationDate:{
+        type:Date
+    }
+})
+
+// Set the 'fullname' virtual property
+UserSchema.virtual('studentFullName').get(function() {
+	return this.studentFirstName + ' ' + this.studentLastName;
+}).set(function(studentFullName) {
+	const splitName = studentFullName.split(' ');
+	this.studentFirstName = splitName[0] || '';
+	this.studentLastName = splitName[1] || '';
+});
+
+// Use a pre-save middleware to hash the password
+// before saving it into database
+UserSchema.pre('save', function(next){
+	//hash the password before saving it
+	this.password = bcrypt.hashSync(this.password, saltRounds);
+	next();
+});
+
+// Create an instance method for authenticating user
+UserSchema.methods.authenticate = function(password) {
+	//compare the hashed password of the database 
+	//with the hashed version of the password the user enters
+	return this.password === bcrypt.hashSync(password, saltRounds);
+};
+
+// Configure the 'UserSchema' to use getters and virtuals when transforming to JSON
+UserSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
+});
+
+// Create the 'Student' model out of the 'Student'
+mongoose.model('Student', StudentSchema);
